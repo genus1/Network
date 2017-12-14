1c064479057736cbcb86d27b4c1102fc75211910
genus1 <roger@rogersbooks.info>
2017-12-13 19:11:09 -0800

    Over the Sholder Camera
    Added a Camera to the Player

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -20,4 +20,9 @@ public class Player : NetworkBehaviour {
 
 		transform.Translate (inputValue);
 	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponentInChildren<Camera>().enabled = true;
+	}
+
 }
